32e1a25d077f5312796b3b208c38ca372bc17a13
unknown <mohammad.balouza@gmail.com>
2023-05-25 03:56:47 +0300

    Backend: Removed the useless CRUDs

--- Raphael-Server/app/crud/crud_tool.py
+++
@@ -1,9 +0,0 @@
-from app.crud.base import CrudBase
-from app.models.tool import Tool
-from app.schemas.tool import ToolCreate, ToolUpdate
-
-class CrudTool(CrudBase[Tool, ToolCreate, ToolUpdate]):
-    pass
-
-
-tool = CrudTool(Tool)
