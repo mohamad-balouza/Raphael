3277cbe94a832e9a435941fa2f9c1fbc7d75fc4a
unknown <mohammad.balouza@gmail.com>
2023-05-25 03:53:17 +0300

    Backend: Removed the useless models

--- Raphael-Server/app/models/__init__.py
+++ Raphael-Server/app/models/__init__.py
@@ -1,10 +1,2 @@
 from .user import User, Base
 from .user_type import UserType
-from .tool import Tool
-from .flag import Flag
-from .used_flag import UsedFlag
-from .openvpn_file import OpenvpnFile
-from .schedule import Schedule
-from .script import Script
-from .favorite_script import FavoriteScript
-from .scheduled_flag import ScheduledFlag
--- Raphael-Server/app/models/user.py
+++ Raphael-Server/app/models/user.py
@@ -3,7 +3,6 @@ from sqlalchemy.orm import relationship
 from typing import TYPE_CHECKING
 
 from app.database.database import Base
-from app.models.favorite_script import FavoriteScript
 
 class User(Base):
     __tablename__ = "users"
@@ -16,8 +15,3 @@ class User(Base):
     user_type_id = Column(Integer, ForeignKey("user_types.id"), index=True, default=2)
 
     user_type = relationship("UserType", back_populates="user")
-    flags_used_by_user = relationship("UsedFlag", back_populates="user_that_used")
-    uploaded_file = relationship("OpenvpnFile", back_populates="user_that_uploaded")
-    schedules = relationship("Schedule", back_populates="user_schedule")
-    scripts = relationship("Script", back_populates="author")
-    favorited_scripts = relationship("FavoriteScript", back_populates="user_who_favorited")
